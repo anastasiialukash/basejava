cb5aa8961effd781358e98a1f1c6bb08dd393f0b
Anastasia Lukash <anastasia16rom@gmail.com>
2021-01-06 12:55:45 +0300

    Methods save, get, delete, clear, getAll, size in ArrayStorage class were implemented

--- src/ArrayStorage.java
+++ src/ArrayStorage.java
@@ -1,3 +1,6 @@
+import java.util.Arrays;
+import java.util.Objects;
+
 /**
  * Array based storage for Resumes
  */
@@ -5,26 +8,57 @@ public class ArrayStorage {
     Resume[] storage = new Resume[10000];
 
     void clear() {
+        for (int i = 0; i < storage.length; i++) {
+            if (storage[i] != null) {
+                storage[i] = null;
+            }
+        }
     }
 
     void save(Resume r) {
+        for (int i = 0; i < storage.length; i++) {
+            if (storage[i] == null) {
+                storage[i] = r;
+                break;
+            }
+        }
     }
 
     Resume get(String uuid) {
+        for (Resume resume : storage) {
+            if (resume.uuid.equals(uuid)) {
+                return resume;
+            } else {
+                System.out.print("Resume is not found\n");
+                return null;
+            }
+        }
         return null;
     }
 
     void delete(String uuid) {
+
+        Resume[] copy = new Resume[storage.length];
+
+        for (int i = 0; i < storage.length; i++) {
+            if (storage[i].uuid.equals(uuid)) {
+                if (storage.length - 1 - i >= 0) System.arraycopy(storage, i + 1, copy, i, storage.length - 1 - i);
+                break;
+            }
+        }
+
+        storage = copy;
     }
+
 
     /**
      * @return array, contains only Resumes in storage (without null)
      */
     Resume[] getAll() {
-        return new Resume[0];
+        return Arrays.stream(storage).filter(Objects::nonNull).toArray(Resume[]::new);
     }
 
     int size() {
-        return 0;
+        return getAll().length;
     }
 }
