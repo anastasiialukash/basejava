7447f08969c3b3afb9bfd8cd360ffb19575f1765
Anastasia Lukash <anastasia16rom@gmail.com>
2021-01-07 16:10:00 +0300

    Methods save, get, delete, clear in ArrayStorage were changed

--- src/ArrayStorage.java
+++ src/ArrayStorage.java
@@ -1,5 +1,6 @@
 import java.util.Arrays;
 import java.util.Objects;
+import java.util.stream.IntStream;
 
 /**
  * Array based storage for Resumes
@@ -8,46 +9,40 @@ public class ArrayStorage {
     Resume[] storage = new Resume[10000];
 
     void clear() {
-        for (int i = 0; i < storage.length; i++) {
-            if (storage[i] != null) {
-                storage[i] = null;
-            }
+        int resumeCount = size();
+        for (int i = 0; i <= resumeCount; i++) {
+            storage[i] = null;
         }
     }
 
     void save(Resume r) {
-        for (int i = 0; i < storage.length; i++) {
-            if (storage[i] == null) {
-                storage[i] = r;
-                break;
-            }
-        }
+        int resumeCount = size();
+        storage[resumeCount] = r;
     }
 
     Resume get(String uuid) {
-        for (Resume resume : storage) {
-            if (resume.uuid.equals(uuid)) {
-                return resume;
-            } else {
-                System.out.print("Resume is not found\n");
-                return null;
+        int count = size();
+        for (int i = 0; i < count; i++) {
+            if (storage[i].uuid.equals(uuid)) {
+                return storage[i];
             }
         }
         return null;
     }
 
     void delete(String uuid) {
+        int count = size();
+        int index = 0;
 
-        Resume[] copy = new Resume[storage.length];
-
-        for (int i = 0; i < storage.length; i++) {
+        for (int i = 0; i < count; i++) {
             if (storage[i].uuid.equals(uuid)) {
-                if (storage.length - 1 - i >= 0) System.arraycopy(storage, i + 1, copy, i, storage.length - 1 - i);
-                break;
+                storage[i] = null;
+                index = i;
             }
         }
 
-        storage = copy;
+        if (storage.length - 1 - index >= 0)
+            System.arraycopy(storage, index + 1, storage, index, storage.length - 1 - index);
     }
 
 
