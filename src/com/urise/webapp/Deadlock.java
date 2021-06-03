c893425c6f1b20b9c0e7303ff03bcb009620e82f
Anastasia Lukash <anastasia16rom@gmail.com>
2021-06-03 22:17:50 +0300

    HW11.2

--- src/com/urise/webapp/Deadlock.java
+++ src/com/urise/webapp/Deadlock.java
@@ -2,48 +2,28 @@ package com.urise.webapp;
 
 public class Deadlock {
 
-    public static final Object LOCK_ONE = new Object();
-    public static final Object LOCK_TWO = new Object();
+    public static final String LOCK_ONE = "LOCK_ONE";
+    public static final String LOCK_TWO = "LOCK_TWO";
 
     public static void main(String[] args) {
-        ThreadOne threadOne = new ThreadOne();
-        ThreadTwo threadTwo = new ThreadTwo();
-
-        threadOne.start();
-        threadTwo.start();
+        deadLock(LOCK_ONE, LOCK_TWO);
+        deadLock(LOCK_TWO, LOCK_ONE);
     }
 
-    private static class ThreadOne extends Thread {
-        public void run() {
-            synchronized (LOCK_ONE) {
-                System.out.println("ThreadOne is holding LOCK_ONE");
+    private static void deadLock(Object lockOne, Object lockTwo) {
+        new Thread(() -> {
+            synchronized (lockOne) {
+                System.out.println("Holding " + lockOne);
                 try {
                     Thread.sleep(1000);
                 } catch (InterruptedException e) {
                     e.printStackTrace();
                 }
-                System.out.println("Waiting...");
-                synchronized (LOCK_TWO) {
-                    System.out.println("ThreadOne is holding LOCK_ONE and LOCK_TWO");
+                System.out.println("Waiting for " + lockTwo);
+                synchronized (lockTwo) {
+                    System.out.println("Holding " + lockTwo);
                 }
             }
-        }
-    }
-
-    private static class ThreadTwo extends Thread {
-        public void run() {
-            synchronized (LOCK_TWO) {
-                System.out.println("ThreadTwo is holding LOCK_ONE");
-                try {
-                    Thread.sleep(1000);
-                } catch (InterruptedException e) {
-                    e.printStackTrace();
-                }
-                System.out.println("Waiting...");
-                synchronized (LOCK_ONE) {
-                    System.out.println("ThreadTwo is holding LOCK_ONE and LOCK_TWO");
-                }
-            }
-        }
+        }).start();
     }
 }
