bebeef502c078db85c2c4fe83b14ee4adf2e5d22
Anastasia Lukash <anastasia16rom@gmail.com>
2021-10-23 00:41:40 +0300

    HW12/13

--- test/com/urise/webapp/storage/AbstractStorageTest.java
+++ test/com/urise/webapp/storage/AbstractStorageTest.java
@@ -1,5 +1,6 @@
 package com.urise.webapp.storage;
 
+import com.urise.Config;
 import com.urise.webapp.ResumeTestData;
 import com.urise.webapp.exception.ExistStorageException;
 import com.urise.webapp.exception.NotExistStorageException;
@@ -14,7 +15,8 @@ import java.util.List;
 
 public abstract class AbstractStorageTest {
 
-    protected static final File STORAGE_DIR = new File("/Users/alukash/Documents/QA/test");
+//    protected static final File STORAGE_DIR = new File("/Users/alukash/Documents/QA/test");
+    protected static final File STORAGE_DIR = Config.get().getStorageDir();
     protected final Storage storage;
 
     private static final String UUID_1 = "uuid1";
